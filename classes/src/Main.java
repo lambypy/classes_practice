3d059b7bd77d307c35b49cd60d7161bcf17157cf
lambypy <danbud@hotmail.co.uk>
2024-10-24 16:50:15 +0100

    Additional Spaceship Details

--- classes/src/Main.java
+++ classes/src/Main.java
@@ -1,9 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         Spaceship Rocinante = new Spaceship();
         System.out.println(Rocinante);
+        Rocinante.setAllegance("Space Yugoslavia");
+        Rocinante.setName("Rocinante");
+        Rocinante.setArmaments(Armaments.HEAVILY_ARMED);
+        Rocinante.setNumOfCrew(115);
+        Rocinante.setHyperdriveActive(true);
+        Rocinante.setOwner("Dan");
+
+        System.out.println(Rocinante.getName() + " belongs to " + Rocinante.getAllegance() + ".");
+        user_request();
+    }
+
+
+    public static void user_request(){
+        /*Takes requests from the user on spaceship information*/
+
+        System.out.println("Ask about details of the spaceship");
+        Scanner userRequestScanner = new Scanner(System.in);
+        String userRequest = userRequestScanner.nextLine();
+        System.out.println(userRequest);
+
     }
 }
